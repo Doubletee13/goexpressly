723ac7e6eda7c18998f3757495b20a04cfeeff34
Akinlade Temitope <akinlade_temitope0506@yahoo.com>
2026-08-05 07:32:36 +0100

    fix(backend): add vercel frontend to CORS allow origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -52,6 +52,7 @@ def create_app() -> FastAPI:
             "http://127.0.0.1:3000",
             "http://localhost:8080",
             "http://127.0.0.1:8080",
+            "https://goexpressly.vercel.app",
         ],
         allow_credentials=True,
         allow_methods=["*"],
